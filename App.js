f6bdf56a04f77cb484d8b44f8307f7419a1bfdaa
zulfiandri09 <zfphone09@gmail.com>
2021-12-19 12:45:20 +0700

    Membuat Component dg Class Component

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Image, Text,TextInput,View } from 'react-native';
 
 const App = () =>{
   return(
     <View>
       <View style={{width:80, height:80, backgroundColor:'#ccc'}}></View>
-      <Text>Zulfi</Text>
+      <Text>Zulfi ok</Text>
       <Text>Andri</Text>
       <ComAndi/>
       <PhotoSaya/>
       <TextInput style={{borderWidth:1}}></TextInput>
+      <BoxGreen />
+      <Profilzf />
     </View>
   );
 }
@@ -21,4 +23,23 @@ const PhotoSaya = () => {
   return <Image source={{uri:'https://bumame.cloud.co.id/bumame_test/assets/media/logos/logo-web.png'}}
   style={{width:278, height:80}}></Image>
 }
+
+class BoxGreen extends Component {
+  render(){
+    return <Text>ini Component dari Class</Text>
+  }
+}
+
+class Profilzf extends Component {
+  render(){
+    return (
+    <View>
+      <Image source={{uri:'https://placeimg.com/100/100/people'}} 
+      style={{width:100, height:100, borderWidth:50}}></Image>
+      <Text style={{color:'blue', fontSize:24}}> Photo Profile</Text>
+    </View>
+    );
+  }
+}
+
 export default App;
